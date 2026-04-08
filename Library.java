4d7bd10b643ecc6f7a6f12ec05fec116baa86102
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-04-08 18:29:22 +0530

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<Book> books;
+
+    public Library(){
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    public List<Book> getBooks(){
+        return books;
+    }
+}
